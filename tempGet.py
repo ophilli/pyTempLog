ce0815d7cf4aac24acdce97ab8cab8a23bb0d68b
Owen Phillips <ophilli@clemson.edu>
2016-10-30 15:19:58 -0400

    Rename Variables to Indicated Extruder Temperatures
    I've renamed all of the variables returned by the REST API so that it
    is clear that these are limited to the Extruder Temperatures.

--- tempGet.py
+++ tempGet.py
@@ -5,8 +5,9 @@ import mysql.connector
 
 logging.basicConfig(filename='temp.log', level=logging.INFO)
 
-api = '/api/printer/tool?history=true&limit=241'
-add_ext = """INSERT INTO `ophilli`.`Temperatures` (`Temp_Actual`, `Temp_Target`, `Serial_Number`, `Time`) VALUES (%s, %s, %s, %s)"""
+api_ext = '/api/printer/tool?history=true'
+
+add_ext = """INSERT IGNORE INTO `ophilli`.`ext_Temp` (`Temp_Actual`, `Temp_Target`, `Mach_Key`, `Time`) VALUES (%s, %s, %s, %s)"""
 
 try:
     cnx = mysql.connector.connect(user='ophilli', password='****', host='sbxmysql.clemson.edu', database='ophilli')
@@ -22,7 +23,6 @@ except mysql.connector.Error as err:
 cursor = cnx.cursor()
 
 
-url = 'http://series1-10306.local:5000/api/printer/tool?history=true'
 headers = { 'X-Api-Key' : '7AA24AC7430A43ABAEA065C83458270C' }
 
 hosts = {
@@ -35,20 +35,17 @@ hosts = {
     }
 
 for i in hosts:
-	resp = requests.get('http://series1-' + str(i) + '.local:5000' + api, headers=headers)
+	respExt = requests.get('http://series1-' + str(i) + '.local:5000' + api_ext, headers=headers)
+	
+	if respExt.status_code != 200:
+	    raise ApiError('EXT ERROR')
 
-	if resp.status_code != 200:
-	    # This means something went wrong
-	    raise ApiError('BLAH')
+	rExt = respExt.json()
 
-	r = resp.json()
+	for j in rExt['history']:
+	    data_ext = (j['tool0']['actual'], j['tool0']['target'], hosts.index(i), datetime.datetime.fromtimestamp(j['time']))
+	    cursor.execute(add_ext, data_ext)
 
-	emp_no = 0
-
-	for j in r['history']:
-	    data_temp = (j['tool0']['actual'], j['tool0']['target'], i, datetime.datetime.fromtimestamp(j['time']))
-	    # print('Temp was: {} at Time: {}'.format(i['tool0']['actual'], datetime.datetime.fromtimestamp(i['time'])))
-	    cursor.execute(add_ext, data_temp)
 
 cnx.commit()
 
