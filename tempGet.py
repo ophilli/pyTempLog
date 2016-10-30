42acad5906b0eb882b63e1de10c896fa9af5c693
Owen Phillips <ophilli@clemson.edu>
2016-10-30 15:23:08 -0400

    Query Bed Temperatures for Print Pod
    I've added the API calls to the print pod that allow us to check the
    bed temperatures. These calls are stored in a separate table in order
    to properly denote what kind of data we are working with.

--- tempGet.py
+++ tempGet.py
@@ -6,8 +6,10 @@ import mysql.connector
 logging.basicConfig(filename='temp.log', level=logging.INFO)
 
 api_ext = '/api/printer/tool?history=true'
+api_bed = '/api/printer/bed?history=true'
 
 add_ext = """INSERT IGNORE INTO `ophilli`.`ext_Temp` (`Temp_Actual`, `Temp_Target`, `Mach_Key`, `Time`) VALUES (%s, %s, %s, %s)"""
+add_bed = """INSERT IGNORE INTO `ophilli`.`bed_Temp` (`Temp_Actual`, `Temp_Target`, `Mach_Key`, `Time`) VALUES (%s, %s, %s, %s)"""
 
 try:
     cnx = mysql.connector.connect(user='ophilli', password='****', host='sbxmysql.clemson.edu', database='ophilli')
@@ -36,16 +38,23 @@ hosts = {
 
 for i in hosts:
 	respExt = requests.get('http://series1-' + str(i) + '.local:5000' + api_ext, headers=headers)
+	respBed = requests.get('http://series1-' + str(i) + '.local:5000' + api_bed, headers=headers)
 	
 	if respExt.status_code != 200:
 	    raise ApiError('EXT ERROR')
+	if respBed.status_code != 200:
+	    raise ApiError('BED ERROR')
 
 	rExt = respExt.json()
+        rBed = respBed.json()
 
 	for j in rExt['history']:
 	    data_ext = (j['tool0']['actual'], j['tool0']['target'], hosts.index(i), datetime.datetime.fromtimestamp(j['time']))
 	    cursor.execute(add_ext, data_ext)
 
+        for k in rBed['history']:
+            data_bed = (k['bed']['actual'], k['bed']['target'], hosts.index(i), datetime.datetime.fromtimestamp(k['time']))
+            cursor.execute(add_bed, data_bed)
 
 cnx.commit()
 
