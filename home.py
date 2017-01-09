0e6c5b1fb781474da4513c4a979ad321a803e8cc
Owen <ophilli@clemson.edu>
2017-01-09 13:10:20 +0000

    home all the things!

---
+++ home.py
@@ -0,0 +1,21 @@
+import logging
+import requests
+import json
+
+api = '/api/printer/printhead'
+
+headersi = { 'Content-Type': 'application/json',
+	    'X-Api-Key' : '7AA24AC7430A43ABAEA065C83458270C' }
+datai = { "command" : "jog", "axes":["x","y","z"]}
+
+hosts = [
+	'repair',
+	10307, 
+	10308, 
+	10309, 
+	10310
+    ]
+
+for i in hosts:
+	resp = requests.post('http://series1-' + str(i) + '.local:5000' + api, data=json.dumps(datai), headers=headersi)
+	print(resp.status_code)
